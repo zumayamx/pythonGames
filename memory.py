f73bb43608c79f5a72004d589bec02cbaf37304d
Diego Valencia <a01784568@tec.mx>
2024-09-12 19:59:15 -0600

    If applied, this commit will center the digits

--- memory.py
+++ memory.py
@@ -1,3 +1,6 @@
+Aquí tienes el código sin comentarios:
+
+```python
 from random import *
 from turtle import *
 
@@ -7,8 +10,7 @@ car = path('car.gif')
 tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
-taps = 0 
-
+taps = 0
 
 def square(x, y):
     up()
@@ -21,18 +23,15 @@ def square(x, y):
         left(90)
     end_fill()
 
-
 def index(x, y):
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
-
 
 def xy(count):
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
-
 def tap(x, y):
     global taps
-    taps += 1  
+    taps += 1
     spot = index(x, y)
     mark = state['mark']
 
@@ -43,10 +42,8 @@ def tap(x, y):
         hide[mark] = False
         state['mark'] = None
 
-
 def all_uncovered():
     return all(not hidden for hidden in hide)
-
 
 def draw():
     clear()
@@ -61,7 +58,7 @@ def draw():
         goto(0, 0)
         write('¡Juego completado!', align='center', font=('Arial', 30, 'bold'))
         update()
-        return 
+        return
 
     for count in range(64):
         if hide[count]:
@@ -73,13 +70,16 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        if tiles[mark] >= 10:
+            goto(x + 7, y + 5)
+        else:
+            goto(x + 17, y + 5)
+
         color('black')
         write(tiles[mark], font=('Arial', 30, 'normal'))
 
     update()
     ontimer(draw, 100)
-
 
 shuffle(tiles)
 setup(420, 420, 370, 0)
@@ -89,3 +89,4 @@ tracer(False)
 onscreenclick(tap)
 draw()
 done()
+```
