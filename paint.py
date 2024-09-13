a920cbd9c4fd43f8fe670a95fcc10abf5393e119
zuamaya <a01784238@tec.mx>
2024-09-12 18:37:24 -0600

    feat: complete function to draw a triangle

--- paint.py
+++ paint.py
@@ -75,9 +75,17 @@ def rectangle(start, end):
 
 
 def triangle(start, end):
-    """Draw triangle from start to end."""
-    pass  # TODO
+    """Dibuja un triángulo equilátero desde start hasta end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
 
+    for count in range(3):
+        forward(end.x - start.x)
+        left(120)
+
+    end_fill()
 
 def tap(x, y):
     """Store starting point or draw shape."""
